dc490783a0d0e0905c4e28cb888244af6b8edd4c
myshyak5 <stameska40@mail.ru>
2024-10-19 17:04:32 +0200

    Add files via upload

---
+++ LAB_2_V8/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+    //Вариант 8
+    unsigned short A;
+    std::cin >> A;
+    std::cout << "V = " << pow(A, 3) << std::endl;
+
+    // //Вариант 10
+    // float N, X;
+    // std::cin >> N >> X;
+    // std::cout << pow(X, 1/N) << std::endl;
+    
+    // //Вариант 5 (Объём прямоугольного параллелепипеда)
+    // int A, B, C;
+    // std::cin >> A >> B >> C;
+
+    // std::cout << "Var5: V = " << A*B*C << std::endl;
+
+    // //Вариант 1
+    // int p = (A+B+C)/2;
+
+    // std::cout << "Var1: S (Geron) = " << sqrt(p * (p - A) * (p - B) * (p - C)) << std::endl;
+
+    // //Вариант 2
+    // int h, a;
+    // std::cin >> h >> a;
+
+    // std::cout << "Var2: S = " << a * h / 2 << std::endl;
+
+    // //Вариант 3
+    // int A1, B1;
+    // std::cin >> A1 >> B1;
+
+    // std::cout << "Var2: S = " << A1 * B1 / 2 << std::endl;
+
+    return 0;
+}
