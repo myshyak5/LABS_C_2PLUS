12bba99a0a47563659293b39a41259b869fe717a
myshyak5 <stameska40@mail.ru>
2024-11-16 21:48:27 +0200

    Add files via upload

---
+++ LAB_8_V7/TASK_1/func.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+const int N_max = 2000;
+
+bool Read(int& n, int& cnt, std::string words[N_max]);
+void ClearElements(int cnt, std::string words[N_max]);
+void IsTwoEqualLetters(std::string words[N_max], char sogli, int& k, int i);
+bool IsEqualLetters(int cnt, std::string words[N_max], int i);
+void NewWords(int& cnt, std::string words[N_max]);
+void CountLettersInWord(int cnt, std::string words[N_max], int CountLetters[N_max]);
+void Sort(int cnt, std::string words[N_max], int CountLetters[N_max]);
+void Write(int n, std::string words[N_max]);
