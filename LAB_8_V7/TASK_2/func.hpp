a66d64a5427630a8c692a3546b84bb3e9668c8aa
myshyak5 <stameska40@mail.ru>
2024-11-23 23:39:25 +0200

    Update func.hpp

--- LAB_8_V7/TASK_2/func.hpp
+++ LAB_8_V7/TASK_2/func.hpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <fstream>
 
-const int N_max = 2000;
+const int N_max = 10248;
 
 bool Read(int& cnt, std::string words[N_max]);
 void ClearElements(int cnt, std::string words[N_max]);
 void ClearGl(int cnt, std::string words[N_max]);
+void ClearEmpty(int& cnt, std::string words[N_max]);
 bool IsPalindromMoreOneLetter(int cnt, std::string words[N_max]);
 void CountOfSogl(int cnt, std::string words[N_max], int CountSogl[N_max]);
 void SortPalindrom(int cnt, std::string words[N_max], int CountSogl[N_max]);
