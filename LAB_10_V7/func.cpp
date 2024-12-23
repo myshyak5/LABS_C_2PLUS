65f8c09f6557dc54c06c8967c2ae6b829a06cd8e
myshyak5 <stameska40@mail.ru>
2024-12-23 13:47:09 +0200

    Update func.cpp

--- LAB_10_V7/func.cpp
+++ LAB_10_V7/func.cpp
@@ -81,14 +81,6 @@ namespace mt
 
     bool IsPrime(int n)
     {
-        if (n == 2)
-        {
-            return true;
-        }
-        else if (n <= 1)
-        {
-            return false;
-        }
         for (int i = 2; i <= sqrt(n); i++)
         {
             if (n % i == 0)
