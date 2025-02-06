1d0f643bc999bca352584a5fe5ef14bf018d2b11
myshyak5 <stameska40@mail.ru>
2025-02-06 23:35:43 +0200

    Add files via upload

---
+++ LAB_12_V7/TASK_1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int Max(int m, int k)
+{
+    if (m > k)
+    {
+        return m;
+    }
+    return k;
+}
+size_t f(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return Max(f(n / 10), n % 10);
+}
+int main()
+{
+    unsigned int n;
+    std::cin >> n;
+    if ((n > 0) && (n < pow(2, 31)))
+    {
+        std::cout << f(n);
+    }
+
+    return 0;
+}
