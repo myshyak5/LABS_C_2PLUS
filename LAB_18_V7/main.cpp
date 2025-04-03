2cbcbc75fc0b91fac0aa97bb0c5c0a2ab739face
myshyak5 <stameska40@mail.ru>
2025-04-03 12:53:24 +0200

    Add files via upload

---
+++ LAB_18_V7/main.cpp
@@ -0,0 +1,38 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({480u, 196u}), "SFML works!");
+
+    sf::CircleShape circle(50.f);
+    sf::RectangleShape rectangle({100.f, 100.f});
+    sf::CircleShape triangle(50.f, 3.f);
+    
+    circle.setFillColor(sf::Color::Green);
+    rectangle.setFillColor(sf::Color::Cyan);
+    triangle.setFillColor(sf::Color::Blue);
+
+    circle.setOrigin({50.f, 50.f});
+    rectangle.setOrigin({50.f, 50.f});
+    triangle.setOrigin({50.f, 37.5f});
+
+    circle.setPosition({50.f, 75.f});
+    rectangle.setPosition({200.f, 75.f});
+    triangle.setPosition({350.f, 75.5f});
+
+    rectangle.setRotation(sf::Angle(sf::degrees(45)));
+    triangle.setRotation(sf::Angle(sf::degrees(90)));
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()){
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(circle);
+        window.draw(rectangle);
+        window.draw(triangle);
+        window.display();
+    }
+}
